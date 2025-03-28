c9df09b9de2e3d936803535703ac0ed304537122
Krosenvag <herve.leguennec13@gmail.com>
2025-03-28 17:54:44 +0100

    fix formatting in appendToEachArrayValue function and update console log in main file

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const newArray = [];
-    for (const element of array) {
-      newArray.push(appendString + element);
-    }
-  
-    return newArray;
+  const newArray = [];
+  for (const element of array) {
+    newArray.push(appendString + element);
   }
+
+  return newArray;
+}
