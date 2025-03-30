18f5fc74b25407f18ce06f7e8069a8ee50a2ebd2
Krosenvag <herve.leguennec13@gmail.com>
2025-03-30 14:29:08 +0200

    refactor Building class to improve error handling and add getter for sqft; implement evacuationWarningMessage in TestBuilding class

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,10 +1,9 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft !== 'number') throw TypeError('sqft must be a number');
+    if (typeof sqft !== 'number') throw new TypeError('sqft must be a number');
     if (
       this.constructor !== Building
-        && this.evacuationWarningMessage
-          === Building.prototype.evacuationWarningMessage
+      && Object.getPrototypeOf(this).evacuationWarningMessage === Building.prototype.evacuationWarningMessage
     ) {
       throw new Error(
         'Class extending Building must override evacuationWarningMessage',
@@ -13,10 +12,12 @@ export default class Building {
     this._sqft = sqft;
   }
 
+  // Getter for sqft
   get sqft() {
     return this._sqft;
   }
 
+  // Abstract method
   evacuationWarningMessage() {
     throw new Error(
       'Class extending Building must override evacuationWarningMessage',
--- ES6_classes/5-main.js
+++ ES6_classes/5-main.js
@@ -1,12 +1,17 @@
 import Building from './5-building.js';
 
 const b = new Building(100);
-console.log(b);
+console.log(b); // Building { _sqft: 100 }
 
-class TestBuilding extends Building {}
+class TestBuilding extends Building {
+  evacuationWarningMessage() {
+    return 'Evacuate the building immediately!';
+  }
+}
 
 try {
-  new TestBuilding(200);
+  const tb = new TestBuilding(200);
+  console.log(tb.evacuationWarningMessage()); // Evacuate the building immediately!
 } catch (err) {
   console.log(err);
 }
