2c35daea0767a719d03c471278e54cf2152d1226
Krosenvag <herve.leguennec13@gmail.com>
2025-04-02 09:24:28 +0200

    refactor: standardize import quotes and improve promise handling in multiple files

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,12 +1,12 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-    const promises = [uploadPhoto(), createUser()];
-    return Promise.all(promises)
+  const promises = [uploadPhoto(), createUser()];
+  return Promise.all(promises)
     .then(([photoResult, userResult]) => {
-        console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
-      })
-      .catch(() => {
-        console.log('Signup system offline');
-      });
+      console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
