e01d8eb0d92d80e422192957ca6cae8d371de04b
Krosenvag <herve.leguennec13@gmail.com>
2025-04-02 23:50:58 +0200

    refactor: update guardrail function to improve error handling and ensure consistent return structure

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,8 +1,13 @@
-export default function guardrail (mathFunction) {
-    const queue = []
-
+export default function guardrail(mathFunction) {
+    const queue = [];
+  
     try {
-        const result = mathFunction;
-        
+      const result = mathFunction();
+      queue.push(result);
+    } catch (error) {
+      queue.push(`Error: ${error.message}`);
+    } finally {
+      queue.push('Guardrail was processed');
     }
-}
+    return queue;
+  }
